5f95199ebf287d5c4dae55e1e9d7b7573096496e
Patrick Burdun <patrickandreiburdun@gmail.com>
2025-03-30 16:24:46 +0300

    Task 6: Added solution for task 6 (sets) Implemented the reunion of the sets using OR, adding and element using OR with a number greater than 255, intersection using AND, complement using NOT, removal of an element using NOT and AND, and difference using NOT and AND.
    Signed-off-by: Patrick Burdun <patrickandreiburdun@gmail.com>

--- labs/lab-04/tasks/sets/support/sets.asm
+++ labs/lab-04/tasks/sets/support/sets.asm
@@ -18,22 +18,33 @@ main:
     PRINTF32 `%u\n\x0`, ebx ; print the second set
 
     ; TODO1: reunion of the two sets
-
+    mov ecx, eax
+    or ecx, ebx
+    PRINTF32 `%u\n\x0`, ecx
 
     ; TODO2: adding an element to a set
-
-
+    or eax, 256
+    PRINTF32 `%u\n\x0`, eax
     ; TODO3: intersection of the two sets
-
+    mov ecx, eax
+    and ecx, ebx
+    PRINTF32 `%u\n\x0`, ecx
 
     ; TODO4: the complement of a set
-
+    mov ecx, eax
+    not ecx
+    PRINTF32 `%u\n\x0`, ecx
 
     ; TODO5: removal of an element from a set
-
+    mov ecx, 1
+    not ecx
+    and eax, edx
+    PRINTF32 `%u\n\x0`, eax
 
     ; TODO6: difference of two sets
-
+    not ebx
+    and eax, ebx
+    PRINTF32 `%u\n\x0`, eax
 
     xor eax, eax
     ret
