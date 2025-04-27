6b6a1f85dee5f15690bc5512b12ef5d4487fa737
Patrick Burdun <patrickandreiburdun@gmail.com>
2025-04-27 15:56:09 +0300

    Lab 07 - Added solution for task 5 - Stack Addressing
    Signed-off-by: Patrick Burdun <patrickandreiburdun@gmail.com>

--- labs/lab-07/tasks/stack-addressing/support/stack-addressing.asm
+++ labs/lab-07/tasks/stack-addressing/support/stack-addressing.asm
@@ -12,23 +12,48 @@ main:
     ; TODO 1: replace every "push" instruction by an equivalent sequence of commands (use direct addressing of memory. Hint: esp)
     mov ecx, NUM
 push_nums:
-    push ecx
+    sub esp, 4
+    mov dword [esp], ecx
     loop push_nums
 
-    push 0
-    push "corn"
-    push "has "
-    push "Bob "
+    sub esp, 4
+    mov dword [esp], 0
+
+    sub esp, 4
+    mov dword [esp], "corn"
+    
+    sub esp, 4
+    mov dword [esp], "has"
+    
+    sub esp, 4
+    mov dword [esp], "Bob"
 
     lea esi, [esp]
     PRINTF32 `%s\n\x0`, esi
 
     ; TODO 2: print the stack in "address: value" format in the range of [ESP:EBP]
     ; use PRINTF32 macro - see format above
+    mov eax, ebp
+    print_stack:
+        PRINTF32 `0x%x: 0x%x\n\x0`, eax, [eax]
+        sub eax, 4
+        cmp eax, ebp
+        jge print_stack
+    
+    
+    lea esi, [esp]
+    PRINTF32 `%s\n\x0`, esi
 
-    ; TODO 3: print the string
+    ; Print the array.
+    add esp, 16
+    mov eax, esp
+    print_array:
+    PRINTF32 `%d \x0`, [eax]
 
-    ; TODO 4: print the array on the stack, element by element.
+    add eax, 4
+    cmp eax, ebp
+    jl print_array
+    PRINTF32 `\n\x0`
 
     ; restore the previous value of the EBP (Base Pointer)
     mov esp, ebp
